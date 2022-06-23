656e9ec1ba0c5ec61097c03c9f1f2d66cb7a363e
hbchaney <hbchaney@gmail.com>
2022-06-23 11:11:19 -0400

    commit

---
+++ notes/linked_nodemap.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+
+
+class Node { 
+
+    int name; 
+    int value; 
+    Node* friends; 
+    int num_friends; 
+
+    public: 
+
+    Node (int n, int v, Node* f = nullptr, int n_f = 0) 
+
+
+};
